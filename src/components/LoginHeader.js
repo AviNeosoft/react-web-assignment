78048c54388e43cd6f997ecbd9d77c87bc90a568
System Administrator <root@XYZ-123.local>
2018-08-21 13:17:04 +0530

    login header implemented

--- src/components/LoginHeader.js
+++ src/components/LoginHeader.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const HeaderContiner = styled.div`
+    background-color: #4e62ff;
+    border-radius: 5px;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    z-index: 0;
+`
+const HeaderTabs = styled.div`
+    display: flex;
+    font-weight: 500;
+    font-style: bold;
+    justify-content: space-around;
+    width: calc(100% - 60px);
+    color: aliceblue;
+    &:hover {
+        cursor: pointer;
+      }
+    
+`
+const SubHeader = HeaderTabs.extend`
+    justify-content: space-around;
+    width: calc(100% - 35em);
+`
 export const LoginHeader =()=>(
-    <div>header</div>
+    <HeaderContiner>
+        <HeaderTabs>
+            Pictagraph
+            <SubHeader>
+                About Us
+                Log In
+            </SubHeader>
+        </HeaderTabs>
+    </HeaderContiner>
 )
