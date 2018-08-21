5ad8a4b9120bf35cd6dc5401c78592a0d1ec5448
System Administrator <root@XYZ-123.local>
2018-08-21 12:24:21 +0530

    package updated login, routes setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch,BrowserRouter as Router } from 'react-router-dom';
 
+import Login from './pages/login'
+//import HomeScreen from './pages/HomeScreen'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+            <Route exact path="/" component={Login} />
+            {/* <Route exact path="/Home" component={HomeScreen} /> */}
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {LoginHeader} from '../components';
+
+export default class Login extends React.Component{
+    render(){
+        return(
+
+            <LoginHeader /> 
+        )
+
+    }
+}
